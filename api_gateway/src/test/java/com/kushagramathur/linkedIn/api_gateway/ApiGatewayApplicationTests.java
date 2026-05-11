252925c1e8ed91831cf2a2c87485426bf85baa45
CoderGhost37 <coderghost37@gmail.com>
2026-05-11 20:14:13 +0530

    feat: init api-gateway and configure eureka clients

---
+++ api_gateway/src/test/java/com/kushagramathur/linkedIn/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushagramathur.linkedIn.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- user_service/src/main/java/com/kushagramathur/linkedIn/user_service/repository/UserRepository.java
+++ user_service/src/main/java/com/kushagramathur/linkedIn/user_service/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.kushagramathur.linkedIn.user_service.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.ScopedValue;
 import java.util.Optional;
 
 @Repository
